02441c62e662d4cb7f91e4c7ff832e57bb7b77b8
GitHub Action <action@github.com>
2024-06-08 18:14:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/olivierhermans/serverlessconverter/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/olivierhermans/serverlessconverter/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.olivierhermans.serverlessconverter.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
